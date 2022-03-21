f8f8f36306686b37ef3a84bd496b0e06db4abe82
junjieweb <297901657@qq.com>
2022-03-22 00:08:16 +0800

    add commit

--- 12_src_自定义hook/App.vue
+++ 12_src_自定义hook/App.vue
@@ -1,15 +1,18 @@
 <template>
   <button @click="isShow = !isShow">切换隐藏/显示</button>
   <Demo v-if="isShow"></Demo>
+  <hr>
+  <Test></Test>
 </template>
 
 <script>
 import Demo from "./components/Demo.vue";
+import Test from "@/components/Test";
 import {ref} from "vue";
 
 export default {
   name: "App",
-  components: {Demo},
+  components: {Demo, Test},
   setup() {
     let isShow = ref(true)
     return {isShow}
